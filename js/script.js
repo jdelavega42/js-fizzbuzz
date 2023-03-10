5ef2efd067cc98c95477b1e8323613c80bef0714
“jdelavega94” <jcdelavega94@gmail.com>
2023-03-10 15:26:31 +0100

    project completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert('ciao');
+// alert('ciao');
+const listElement = document.querySelector('.container');
+console.log(listElement);
+for (let x = 1; x <= 100; x++ ) {
+    let result;
+    let classColor;
+    if (x % 15 === 0){
+        console.log("FizzBuzz")
+        result = "FizzBuzz";
+        classColor = "bg-danger";
+    } else if (x % 5 === 0) {
+        console.log("Buzz");
+        result = "Buzz";
+        classColor = "bg-warning";
+    } else if (x % 3 === 0) {
+        console.log("Fizz");
+        result = "Fizz";
+        classColor = "bg-success";
+    } else {
+    console.log(x);
+    result = x;
+    classColor = "bg-primary";
+    }
+    listElement.innerHTML += `<div class="ms_box ${classColor}">${result}</div>`
+}
